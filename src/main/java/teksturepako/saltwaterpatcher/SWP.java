fd444f6c968a18ce5cba7b8b3e5ae33d3434d700
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2023-07-18 15:46:57 +0200

    Fix problems with refmap loading; Bump version to 1.5

--- src/main/java/teksturepako/saltwaterpatcher/SWP.java
+++ src/main/java/teksturepako/saltwaterpatcher/SWP.java
@@ -15,7 +15,7 @@ public class SWP {
 
     public static final String MOD_ID = "saltwaterpatcher";
     public static final String MOD_NAME = "SaltWaterPatcher";
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.5";
     public static final String DEPENDENCIES = "required-after:mixinbooter;"
             + "after:simpledifficulty;"
             + "after:biomesoplenty;"
